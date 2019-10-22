2a38ab070245222ba475eba228121c3f7f46a54f
yjq <958109727@qq.com>
2019-10-22 18:02:52 +0800

    123

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,14 +1,59 @@
-@extends('admin.base')
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <style>
+        .tab-2 {
+            font-size: 0;
+            padding: 0 1rem;
+        }
 
-@section('content')
-    <a href="" onclick="test_js()">123</a>
-   <div > <img width="100" height="100" src="/images/1.png" alt="" onclick="showImg(this)"> 213</div>
-@endsection
+        .tab-2 > div {
+            width: 50%;
+            display: inline-block;
+            vertical-align: top;
+            font-size: 0.7rem;
+            text-align: center;
+        }
 
-@section('script')
-    <script>
-        layui.use(['index', 'console']);
+        .tab-3 {
+            font-size: 0;
+            padding: 0 1rem;
+        }
 
-    </script>
+        .tab-3 > div {
+            width: 33.3%;
+            display: inline-block;
+            vertical-align: top;
+            font-size: 0.7rem;
+            text-align: center;
+        }
 
-@endsection
+        .tab-4 {
+            font-size: 0;
+            padding: 0 1rem;
+            background-color: gray;
+        }
+
+        .tab-4 > div {
+            width: 25%;
+            display: inline-block;
+            vertical-align: top;
+            font-size: 0.7rem;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<div class="tab-4" style="width: 200px">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+</div>
+
+
+</body>
+</html>
